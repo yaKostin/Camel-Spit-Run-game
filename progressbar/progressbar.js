094ff7f54ef5a8471c8ae11b3b89ddb9347255da
Daniil Basanets <daniil.basanets@gmail.com>
2015-04-06 19:16:03 +0300

    fixed bug of water<0

--- progressbar/progressbar.js
+++ progressbar/progressbar.js
@@ -1,5 +1,5 @@
 var ProgressBar = function (elementName) {
-    this.value = 100,
+    this.value = 20,
         this.progressBarElement = $(elementName);
 };
 
@@ -15,4 +15,7 @@ ProgressBar.prototype = {
         var width = this.value * bar.width() / 100;
         bar.find('div').animate({width: width}, 500).html(this.value + "%&nbsp;");
     },
+    getValue: function(){
+        return this.value;
+    }
 };
